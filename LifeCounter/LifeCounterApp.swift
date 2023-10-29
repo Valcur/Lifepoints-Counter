0ba8d73671d1f3dbc23cd683df990fdec71f05df
Loic D <loic.danj@gmail.com>
2023-10-29 14:38:18 +0100

    Initial Commit

---
+++ LifeCounter/LifeCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeCounterApp.swift
+//  LifeCounter
+//
+//  Created by Loic D on 29/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct LifeCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
